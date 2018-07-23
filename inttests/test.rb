6ea9a91ed538c6e727aa02f141578e85842248fa
Derrick Sutherland <dsutherland@shadow-soft.com>
2018-07-23 10:51:56 -0400

    Added 4 more tests

--- inttests/test.rb
+++ inttests/test.rb
@@ -2,3 +2,22 @@ describe port(8080) do
   it { should be_listening }
   its('protocols') {should include 'tcp'}
 end
+describe http('http://localhost:8080/PolishedDemo') do
+  its('status') { should cmp 200 }
+end
+describe http('http://localhost:8080/RestWebServices/atickets') do
+  its('status') { should cmp 200 }
+  its('headers.Content-Type') { should cmp 'application/json' }
+end
+describe http('http://localhost:8080/RestWebServices/btickets') do
+  its('status') { should cmp 200 }
+  its('headers.Content-Type') { should cmp 'application/json' }
+end
+describe http('http://localhost:8080/RestWebServices/ctickets') do
+  its('status') { should cmp 200 }
+  its('headers.Content-Type') { should cmp 'application/json' }
+end
+describe http('http://localhost:8080/SoapWebServices/ShiftWs?wsdl') do
+  its('status') { should cmp 200 }
+  its('headers.Content-Type') { should cmp 'text/xml;charset=UTF-8' }
+end
